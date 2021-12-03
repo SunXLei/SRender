049266a9a7069278d7f553101bafe99cad9c6851
Pierce <60810304+Pierce-qiang@users.noreply.github.com>
2021-12-03 12:02:10 +0800

    Add files via upload

---
+++ shader/shader.h
@@ -0,0 +1,88 @@
+#pragma once
+#include "../core/macro.h"
+#include "../core/maths.h"
+#include "../core/model.h"
+#include "../core/camera.h"
+
+struct light
+{
+	vec3 pos;
+	vec3 intensity;
+};
+
+typedef struct cubemap 
+{
+	TGAImage *faces[6];
+}cubemap_t;
+
+typedef struct iblmap 
+{
+	int mip_levels;
+	cubemap_t *irradiance_map;
+	cubemap_t *prefilter_maps[15];
+	TGAImage *brdf_lut;
+} iblmap_t;
+
+typedef struct
+{
+	//light_matrix for shadow mapping, (to do)
+	mat4 model_matrix;
+	mat4 camera_view_matrix;
+	mat4 light_view_matrix;
+	mat4 camera_perp_matrix;
+	mat4 light_perp_matrix;
+	mat4 mvp_matrix;
+
+	Camera *camera;
+	Model *model;
+
+	//vertex attribute
+	vec3 normal_attri[3];
+	vec2 uv_attri[3];
+	vec3 worldcoord_attri[3]; 
+	vec4 clipcoord_attri[3];
+
+	//for homogeneous clipping
+	vec3 in_normal[MAX_VERTEX];
+	vec2 in_uv[MAX_VERTEX];
+	vec3 in_worldcoord[MAX_VERTEX]; //no need to store world data, but the cost is little
+	vec4 in_clipcoord[MAX_VERTEX];
+	vec3 out_normal[MAX_VERTEX];
+	vec2 out_uv[MAX_VERTEX];
+	vec3 out_worldcoord[MAX_VERTEX];
+	vec4 out_clipcoord[MAX_VERTEX];
+
+	//for image-based lighting
+	iblmap_t *iblmap;
+}payload_t;
+
+class IShader
+{
+public:
+	payload_t payload;
+	virtual void vertex_shader(int nfaces, int nvertex) {}
+	virtual vec3 fragment_shader(float alpha, float beta, float gamma) { return vec3(0, 0, 0); }
+};
+
+class PhongShader:public IShader
+{
+public:
+	void vertex_shader(int nfaces, int nvertex);
+	vec3 fragment_shader(float alpha, float beta, float gamma);
+
+};
+
+class PBRShader :public IShader
+{
+public:
+	void vertex_shader(int nfaces, int nvertex);
+	vec3 fragment_shader(float alpha, float beta, float gamma);
+	vec3 direct_fragment_shader(float alpha, float beta, float gamma);
+};
+
+class SkyboxShader :public IShader
+{
+public:
+	void vertex_shader(int nfaces, int nvertex);
+	vec3 fragment_shader(float alpha, float beta, float gamma);
+};
